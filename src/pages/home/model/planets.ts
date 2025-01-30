b11c747d52aadabd109545c1ca538a29a4e3e3b5
Safiro <altxafiro@gmail.com>
2025-01-31 00:40:25 +0300

    feat: Add planets data

---
+++ src/pages/home/model/planets.ts
@@ -0,0 +1,17 @@
+import planetsJson from './planets.json';
+
+type Galaxy = 'Andromeda-IV' | 'Sombrero' | 'Virgo A';
+
+type Climate = 'Polar' | 'Temperate' | 'Tropical';
+
+export type Planet = {
+  name: string;
+  galaxy: Galaxy;
+  diameter: number;
+  dayLength: number;
+  avgTemperature: [number, number];
+  climate: Climate;
+  image: string;
+};
+
+export const planets = planetsJson as unknown as Planet[];
