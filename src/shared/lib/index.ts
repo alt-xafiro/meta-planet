2e1b77deeceb58dab9a7a1985f567d7f042d47a3
Safiro <altxafiro@gmail.com>
2025-02-12 10:22:39 +0300

    chore(ui): Remove unused code

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,7 +1,3 @@
 export { getRootProperty, setNodeProperty, setRootProperty } from './dom/dom';
 export { getIndexWithinLimit, getRandomNumber } from './numbers/numbers';
 export type { CustomComponentProps } from './props/props';
-export {
-  Breakpoint,
-  getBreakpointValue
-} from './tailwind-config/tailwind-config';
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,5 @@
 export { aBeeZee, shareTechMono, IBMPlexMono } from './fonts/fonts';
 export { ExternalLink } from './ExternalLink/ExternalLink';
 export { Logo } from './Logo/Logo';
-export { useMatchMedia } from './useMatchMedia/useMatchMedia';
 export { useMatrixText } from './useMatrixText/useMatrixText';
 export { useMouseParallax } from './useMouseParallax/useMouseParallax';
